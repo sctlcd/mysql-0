a7e3491e876a72fcb16266b7008af8e4b85d5cd9
8 <lucinda.souchet@outlook.com>
2020-07-01 00:19:02 +0100

    add mysql-from-python-03-create_a_table.py

---
+++ mysql-from-python-03-create_a_table.py
@@ -0,0 +1,27 @@
+import os
+import datetime
+import pymysql
+from os import path
+if path.exists("env.py"):
+    import env
+
+
+# Call username and password from env
+username = os.environ.get("root")
+password = os.environ.get("root")
+
+# Connect to the database ans pass in username and password
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password=password,
+                             db='Chinook')
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                        Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
+finally:
+    # Close the connection, regardless of whether or not the above was successful
+    connection.close()
